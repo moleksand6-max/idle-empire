4816b0a4ffb93adbc0479da164ff78914004f47b
moleksand6-max <moleksand6@gmail.com>
2026-04-04 02:46:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 let coins = 0;
+let income = 1; // доход в секунду
 
-function addCoins() {
-  coins++;
+// загрузка сохранения
+if (localStorage.getItem("coins")) {
+  coins = Number(localStorage.getItem("coins"));
+}
+
+if (localStorage.getItem("income")) {
+  income = Number(localStorage.getItem("income"));
+}
+
+function updateUI() {
   document.getElementById("coins").innerText = coins;
+}
+
+// кнопка получения монет
+function addCoins() {
+  coins += income;
+  updateUI();
+  save();
+}
+
+// пассивный доход (каждую секунду)
+setInterval(() => {
+  coins += income;
+  updateUI();
+  save();
+}, 1000);
+
+// сохранение
+function save() {
+  localStorage.setItem("coins", coins);
+  localStorage.setItem("income", income);
 }
